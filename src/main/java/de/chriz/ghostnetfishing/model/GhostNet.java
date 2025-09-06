d3a861da291a67b5e1f1a68839c360c3aded1ab6
ChriZ <chriz@example.com>
2025-09-06 18:40:12 +0200

    feat(form): GPS-Koordinaten auf zwei Felder (Lat/Long) umgestellt + Validierung

--- src/main/java/de/chriz/ghostnetfishing/model/GhostNet.java
+++ src/main/java/de/chriz/ghostnetfishing/model/GhostNet.java
@@ -1,17 +1,27 @@
 package de.chriz.ghostnetfishing.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.*;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class GhostNet {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id; // ID des Netzes
-	@Column(name = "gps_coordinates") // Verbindung zur Datenbank-Spalte
-	private String gpsCoordinates; // Koordinaten des Netzes
-	private float size; // Netzgröße in Quadratmetern
-	@ManyToOne
+	@Column(name = "latitude", nullable = false) // Verbindung zur Datenbank-Spalte
+	private Double latitude; // Koordinaten des Netzes
+	@Column(name = "longitude", nullable = false)
+	private Double longitude; // Koordinaten des Netzes
+	private Float size; // Netzgröße in Quadratmetern
+	@LastModifiedDate
+	@Column(name = "last_updated")
+	private LocalDateTime lastUpdated;
+	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "user_id")
 	private User user; // Nutzer Objekt, beinhaltet alle nutzerbezogenen Variablen
 
@@ -23,10 +33,12 @@ public class GhostNet {
 
 	}
 
-	public GhostNet(String gpsCoordinates, float size, GhostNetStatus status, User user) {
-		this.gpsCoordinates = gpsCoordinates;
+	public GhostNet(Double latitude, Double longitude, float size, GhostNetStatus status, LocalDateTime lastUpdated, User user) {
+		this.latitude = latitude;
+		this.longitude = longitude;
 		this.size = size;
 		this.status = status;
+		this.lastUpdated = lastUpdated;
 		this.user = user;
 	}
 
@@ -57,6 +69,14 @@ public class GhostNet {
 
 	public void setStatus(GhostNetStatus status) {
 		this.status = status;
+	}
+
+	public LocalDateTime getLastUpdated() {
+		return lastUpdated;
+	}
+
+	public void setLastUpdated(LocalDateTime lastUpdated) {
+		this.lastUpdated = lastUpdated;
 	}
 
 	public User getUser() {
